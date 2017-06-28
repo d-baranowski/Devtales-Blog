e1c0d9eab9438fdd25fedab4b59006d460379f27
= <=>
2017-06-28 23:44:09 +0100

    Simple server side rendered navbar

--- src/main/js/app/components/PageNavigation.js
+++ src/main/js/app/components/PageNavigation.js
@@ -2,12 +2,13 @@ import React from "react";
 import {NavLink} from "react-router-dom";
 
 class PageNavigation extends React.Component {
+
     render() {
         return (
             <header>
                 <img id="logo" src="img/devtalesico.svg"/>
                 <nav>
-                    <div className="title">Devtales</div>
+                    <img id="title" src="img/title.svg"/>
                     <ul>
                         <li><NavLink activeClassName="active" exact to="/">Blog</NavLink></li>
                         <li><NavLink activeClassName="active" to="/projects">Projects</NavLink></li>
