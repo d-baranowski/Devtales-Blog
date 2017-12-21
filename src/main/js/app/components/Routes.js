e2dc03e64abc35adfd82f23d78e685f9fe03e350
= <=>
2017-12-21 09:14:04 +0000

    Added not found component

--- src/main/js/app/components/Routes.js
+++ src/main/js/app/components/Routes.js
@@ -4,6 +4,7 @@ import ArticleListContainer from "../containers/articleListContainer";
 import ArticleReaderContaier from "../containers/ArticleReaderContainer";
 import AboutMe from "./AboutMe";
 import Projects from "./Projects";
+import NotFound from "./NotFound";
 
 const Routes = (props) => {
     return (
@@ -15,6 +16,7 @@ const Routes = (props) => {
         <Route path="/article/:articleSlug" component={ArticleReaderContaier}/>
         <Route path="/about" component={AboutMe}/>
         <Route path="/projects" component={Projects}/>
+        <Route path="/not-found" component={NotFound}/>
     </div>
     )
 };
