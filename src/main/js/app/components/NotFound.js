96ca482d8b0c2c7b6cc251e5a2390b71df53c9ef
= <=>
2017-12-21 09:14:04 +0000

    Added not found component

---
+++ src/main/js/app/components/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotFound = (props) => {
+
+    return (
+        <div className="article-container">
+            <img src="/img/travolta.gif"/>
+            <h3>Page Not Found</h3>
+        </div>
+    )
+};
+
+export default NotFound;
