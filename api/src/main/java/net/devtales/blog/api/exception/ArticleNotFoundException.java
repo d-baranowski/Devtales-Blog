0141739d9a098897be1f0dbe34f38c418ddf0ae4
Daniel Baranowski <daniel.baranowski@infinityworks.com>
2018-12-10 22:32:24 +0000

    Deleted backed as the site will the now hosted on S3 to reduce price and complexity

--- api/src/main/java/net/devtales/blog/api/exception/ArticleNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package net.devtales.blog.api.exception;
-
-public class ArticleNotFoundException extends RuntimeException {
-    public ArticleNotFoundException(Long id) {
-        super("Article with id " + id + " wasn't found");
-    }
-}
--- api/src/main/java/net/devtales/blog/api/repository/TagRepository.java
+++
@@ -1,8 +0,0 @@
-package net.devtales.blog.api.repository;
-
-import net.devtales.blog.api.model.Tag;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TagRepository extends CrudRepository<Tag, Long> {
-    public Tag findByValue(String value);
-}
