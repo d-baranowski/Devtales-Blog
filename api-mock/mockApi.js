60e699364e9cd18672cb4bce2421cf05d80a07e4
Daniel Baranowski <daniel.baranowski@infinityworks.com>
2018-12-13 23:32:50 +0000

    Create mock api for development work

---
+++ api-mock/mockApi.js
@@ -0,0 +1,12 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('api.json');
+const middlewares = jsonServer.defaults();
+
+server.use(jsonServer.rewriter({
+    '/api/article/*': '/example',
+    '/api/*': '/$1',
+}));
+server.use(middlewares);
+server.use(router);
+server.listen(8080);
