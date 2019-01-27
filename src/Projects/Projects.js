c670d7f661e9438a0d98092b5cada9db4faadf45
Daniel Baranowski <daniel.baranowski@infinityworks.com>
2019-01-27 11:49:26 +0000

    Add asteroideatch-match to projects

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -30,6 +30,15 @@ export const Projects = () => {
 
     return (
         <div className="tile-parent">
+            <Tile title="AsteroiDeatch Match"
+                  repo="https://devtales.net/projects/asteroideatch-match/index.html"
+                  imageSrc="https://devtales.net/img/asteroideatch-match.gif">
+                <p>
+                    Little demo I've build to try out WebRTC DataChannel. Each time someone visits the page a random
+                    string is generated. When a second person enters the page with the same random string then they'll
+                    join the fun.
+                </p>
+            </Tile>
             <Tile title="Gravity Balls"
                   repo="https://devtales.net/projects/gravity-balls/index.html"
                   imageSrc="https://devtales.net/img/gravity-balls.gif">
